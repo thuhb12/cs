bf023264413a842bb38f7ac9a6f0a93c32c58d48
thuhb12 <dmehb12399@163.com>
2020-08-25 17:55:48 +0800

    golang learn

---
+++ leetcode/golang/slice/sum.go
@@ -0,0 +1,9 @@
+package slice
+
+func Sum(numbers [5]int) (sum int)  {
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
+	}
+	return
+
+}
---
+++ leetcode/src/NotComplete/NotComplete.java
@@ -0,0 +1,5 @@
+package NotComplete;
+
+public class NotComplete {
+
+}
